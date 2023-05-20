5f5ac26e956ff3471b10d844a875a94249c8b29d
yash <yashg7838@gmail.com>
2023-05-20 20:54:47 +0530

    Initial commit

---
+++ lib/screen/login.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class login extends StatelessWidget {
+  const login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
